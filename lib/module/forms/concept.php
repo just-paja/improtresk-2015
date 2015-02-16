2ce3e7e3cdd15ed350eb5b153a3532e1a787c4bc
just-paja <just.paja@gmail.com>
2015-02-16 02:08:50 +0100

    Frontend site base

--- lib/module/forms/concept.php
+++ lib/module/forms/concept.php
@@ -1,3 +1,8 @@
 <?
 
-$this->partial('forms/concepts');
+$end = new DateTime("2015-02-13 20:00:00+01:00");
+$now = new DateTime();
+
+$this->partial('forms/concepts', array(
+	"show_form" => $now <= $end
+));
