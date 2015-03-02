4742ed8c48aea3fe7365df42456983388045655a
just-paja <just.paja@gmail.com>
2015-03-02 11:38:14 +0100

    Admin detail

--- lib/module/car/offer/admin/detail.php
+++ lib/module/car/offer/admin/detail.php
@@ -10,12 +10,25 @@ $offer = \Car\Offer::get_first()
 	->fetch();
 
 if ($offer) {
-	$res->subtitle = $offer->driver.' vás zve na cestu na Improtřesk 2015';
+	$cfg = $rq->fconfig;
+
+	$cfg['ui']['data'] = array(
+		array(
+			'model' => 'Car.Offer',
+			'items' => array(
+				$offer->to_object_with_perms($rq->user)
+			)
+		)
+	);
+
+	$rq->fconfig = $cfg;
+	$res->subtitle = 'úpravy nabídky na sdílení auta';
 
 	$this->partial('pages/carshare-detail', array(
 		"item"     => $offer,
 		"free"     => $offer->seats - $offer->requests->where(array('status' => 2))->count(),
-		"form"     => true,
-		"requests" => $offer->requests->where(array('status' => array(1,2)))->fetch()
+		"form"     => false,
+		"requests" => $offer->requests->where(array('status' => array(1,2)))->fetch(),
 	));
+
 } else throw new \System\Error\NotFound();
