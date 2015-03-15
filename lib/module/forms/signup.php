e0457172b1be1a929a7e02f97295894ed5f1d6e6
just-paja <just.paja@gmail.com>
2015-03-15 18:06:47 +0100

    Nicer time

--- lib/module/forms/signup.php
+++ lib/module/forms/signup.php
@@ -82,7 +82,7 @@ namespace Module\Forms
 
 		public function run()
 		{
-			$start = new \DateTime("2015-03-10 17:00:00+01:00");
+			$start = new \DateTime("2015-03-10 20:00:00+01:00");
 			$end   = new \DateTime("2015-04-20 20:00:00+01:00");
 			$now   = new \DateTime();
 
