72d06129cdbd3bede58c4ea72052eba7141ba5b9
just-paja <just.paja@gmail.com>
2015-03-17 17:47:10 +0100

    Even more stats

--- lib/module/stats/signups.php
+++ lib/module/stats/signups.php
@@ -6,13 +6,29 @@ namespace Module\Stats
 	{
 		public function run()
 		{
+			$account_pay = \Workshop\Check::get_all()
+					->add_cols(array('SUM(amount) as `total`'))
+					->where(array("is_paid" => false))
+					->assoc_with(null)
+					->fetch_one();
+
+			$account_sum = \Workshop\Payment::get_all()
+					->add_cols(array('SUM(amount) as `total`'))
+					->assoc_with(null)
+					->fetch_one();
+
 			$stats = array(
 				"total"   => \Workshop\SignUp::get_all()->count(),
-				"unpaid"    => \Workshop\SignUp::get_all()->where(array('paid' => false))->count(),
+				"unpaid"  => \Workshop\SignUp::get_all()->where(array('paid' => false))->count(),
 				"paid"    => \Workshop\SignUp::get_all()->where(array('paid' => true))->count(),
 				"waiting" => \Workshop\SignUp::get_all()->where(array('paid' => true, 'solved' => false))->count(),
 				"solved"  => \Workshop\SignUp::get_all()->where(array('solved' => true))->count(),
+				"expected_cnt"  => \Workshop\Check::get_all()->where(array("is_paid" => false))->count(),
+				"expected_sum"  => $account_pay['total'],
+				"received_cnt"  => \Workshop\Payment::get_all()->count(),
+				"received_sum"  => $account_sum['total'],
 			);
+
 
 			$ws = \Workshop::get_all()->fetch();
 
